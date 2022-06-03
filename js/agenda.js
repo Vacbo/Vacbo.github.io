37572d0edefd296ce3999e88342a5b0def5bc604
Vacbo <pedrobp1@al.insper.edu.br>
2022-06-03 09:32:37 -0300

    agenda pop up

---
+++ js/agenda.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    editars = document.querySelectorAll('.editar');
+    for (editar of editars) {
+        editar.addEventListener('click', function(event) {
+            t = event.currentTarget
+            
+        }
+    
+    }
+
+
+
+
+
+
+})

4f2e82bb3a15e86727b13d454f9539f2e73399d4
Vacbo <pedrobp1@al.insper.edu.br>
2022-06-08 15:28:36 -0300

    agenda input almost complete

--- js/agenda.js
+++ js/agenda.js
@@ -1,10 +1,46 @@
 document.addEventListener('DOMContentLoaded', function() {
-    editars = document.querySelectorAll('.editar');
+    editars = document.querySelectorAll('.editar')
+    popUp = document.querySelector('.pop-up')
+    adicionar = document.querySelector('.add')
+    input = document.querySelector('.input')
+    function changePosition(t){
+       id =  t.getAttribute('id')
+       if (id == 'seg'){
+            popUp.setAttribute('id', 'segunda')  
+       }
+       else if (id == 'ter'){
+            popUp.setAttribute('id', 'terca')  
+       }
+       else if (id == 'qua'){
+            popUp.setAttribute('id', 'quarta')  
+       }
+       else if (id == 'qui'){
+            popUp.setAttribute('id', 'quinta')  
+       }
+       else if (id == 'sex'){
+           popUp.setAttribute('id','sexta')
+       }
+    }
+
+    function openPopUp() {
+        popUp.style.display = "block";
+    }
+
+    function closePopUp() {
+        popUp.style.display = "none";
+    }
+
     for (editar of editars) {
         editar.addEventListener('click', function(event) {
             t = event.currentTarget
-            
-        }
+            changePosition(t)
+            openPopUp()
+            adicionar.addEventListener('click', function(event) {
+                localStorage.setItem("Evento",input.value)
+                closePopUp()
+            })
+
+        })
     
     }
 
